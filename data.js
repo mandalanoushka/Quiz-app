4d9601eb9ce579bf6677fecd9819fb80cf9ae10c
Anoushka Mandal <anoushkamandal0@gmail.com>
2025-07-26 00:28:07 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+export const data=[
+    {
+        question: "Which device is required for the  internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which continent has the highest number of countroies?",
+        option1: "Asia",
+        option2: "Africa",
+        option3: "Europe",
+        option4: "North America",
+        ans: 2,
+    },
+    {
+        question: "Which is the Largest country in the world?",
+        option1: "China",
+        option2: "India",
+        option3: "Russia",
+        option4: "USA",
+        ans: 3,
+    }
+]
